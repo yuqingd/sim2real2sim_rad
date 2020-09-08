0ad7e40778456c8f60ff70dc45c474eb0ac4c7f0
Yuqing Du <yuqing_du@hotmail.com>
2020-09-08 12:23:50 -0700

    Add testing script

--- scripts/s2r2s.sh
+++ scripts/s2r2s.sh
@@ -1,12 +1,2 @@
-CUDA_VISIBLE_DEVICES=0 python train.py \
-	--domain_name pick-place-v1 \
-	--task_name pick-place \
-	--cameras 2 \
-	--observation_type pixel \
-	--encoder_type pixel \
-	--save_tb --save_video --save_model \
-	--pre_transform_image_size 100 --image_size 84 \
-	--work_dir ./data/pick-place \
-	--agent curl_sac --frame_stack 3 \
-	--seed -1 --critic_lr 1e-3 --actor_lr 1e-3 \
-	--eval_freq 10000 --batch_size 128 --num_train_steps 2000000
+#### ORACLE
+LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libGLEW.so CUDA_VISIBLE_DEVICES=0 python train.py --domain_name metaworld --task_name stick-pull --action_repeat 2 --seed 0 --batch_size 128 --data_augs crop-translate --id temp
