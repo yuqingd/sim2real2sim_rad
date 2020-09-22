83a0d68ac40cb2a007c8d2af27d4d34e6cb5e85c
Yuqing Du <yuqing_du@hotmail.com>
2020-09-22 07:48:10 -0700

    reduce OL3 range

--- sim_param_model.py
+++ sim_param_model.py
@@ -88,7 +88,7 @@ class SimParamModel(nn.Module):
 
 
     def train_classifier(self, obs_traj, sim_params, distribution_mean):
-        dist_range = 10 * torch.FloatTensor(distribution_mean)
+        dist_range = torch.FloatTensor(distribution_mean)
         sim_params = torch.FloatTensor(sim_params)  # 1 - dimensional
         eps = 1e-3
         low = torch.FloatTensor(
