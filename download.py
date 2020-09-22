71cdfe4f7ac0ba0100f12bff11b9c3b542066da2
AliengirlLiv <oliviagracewatkins@gmail.com>
2020-09-22 10:49:11 -0700

    add download script

--- download.py
+++ download.py
@@ -1,18 +1,19 @@
 import argparse
 import os
 
-import paramiko
-from scp import SCPClient
+# import paramiko
+# from scp import SCPClient
+from shutil import copy
 
 # Source: https://stackoverflow.com/questions/250283/how-to-scp-in-python
-def createSSHClient(server, port, user, password):
-# def createSSHClient(location):
-    client = paramiko.SSHClient()
-    client.load_system_host_keys()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(server, port, user, password)
-    # client.connect(location)
-    return client
+# def createSSHClient(server, port, user, password):
+# # def createSSHClient(location):
+#     client = paramiko.SSHClient()
+#     client.load_system_host_keys()
+#     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+#     client.connect(server, port, user, password)
+#     # client.connect(location)
+#     return client
 
 # Collect filename from input
 parser = argparse.ArgumentParser(description="Download last video to Olivia's computer")
@@ -35,12 +36,16 @@ for file_name in files:
     last_video = videos[0]
     print("downloading ", last_video)
 
-    # Download it to my machine
 
-    ssh = createSSHClient(server, port, user, password)
-    scp = SCPClient(ssh.get_transport())
-    last_video_download_name = args.pattern + last_video[last_video.index('real'):]
-    scp.put(last_video, "~/Sim2Real/Sim2/" + last_video_download_name)
+    # Change of plans.  Since IDK an easy way to scp it down, I'm just gonna move it to a new directory and scp from there.
+    directory = "download_files"
+    copy(last_video, directory)
+
+    # # Download it to my machine
+    # ssh = createSSHClient(server, port, user, password)
+    # scp = SCPClient(ssh.get_transport())
+    # last_video_download_name = args.pattern + last_video[last_video.index('real'):]
+    # scp.put(last_video, "~/Sim2Real/Sim2/" + last_video_download_name)
 
 
 # TEST_metaworld-faucet-open-09-21-im84-b128-s0-curl_sac-pixel-crop
