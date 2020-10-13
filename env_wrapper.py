d584beafe2c976c116c1e7b80bfa805189513db9
AliengirlLiv <oliviagracewatkins@gmail.com>
2020-10-12 23:53:48 -0700

    add update back

--- env_wrapper.py
+++ env_wrapper.py
@@ -1175,7 +1175,7 @@ class DR_Dummy(DR_Env):
         self.timestep = 1
         return state, obs
 
-    def render(self, mode, size=None, *args, **kwargs):
+    def render(self, mode='rgb_array', size=None, *args, **kwargs):
         if size is None:
             size = self._size
         rgb_array = np.zeros((64, 64, 3))
@@ -1185,7 +1185,7 @@ class DR_Dummy(DR_Env):
         y_end = min(63, int(np.floor(self.square_y + self.square_size)))
         rgb_array[y_start:y_end, x_start:x_end] = np.clip(np.array([self.square_r, self.square_g, self.square_b]), 0, 1)
         if size is not None:
-            rgb_array = cv2.resize(rgb_array, size)
+            rgb_array = cv2.resize(rgb_array, size).astype(np.uint8)
         return np.transpose(rgb_array, (2, 0, 1))
 
 
