66c94c61797e3c21e9e3cd0ea16912ec3e8be772
AliengirlLiv <oliviagracewatkins@gmail.com>
2020-10-14 15:34:41 -0700

    divide imgs by 255

--- sim_param_model.py
+++ sim_param_model.py
@@ -111,7 +111,10 @@ class SimParamModel(nn.Module):
                 # Input is b x c * num_frames x h x w.  We take the first image and downsample to
                 downsampled_img = input[:, 0, ::25, ::25].reshape(len(input), -1)
                 features = downsampled_img / 255
+                # if torch.max(input).item() > 1:
+                #     input = input / 255
                 #features = self.encoder(input, detach=False)
+
                 print("NORM", torch.norm(features).item(), torch.min(features).item(), torch.max(features).item())
                 if np.random.uniform() < .05:
                     print("features", features)
