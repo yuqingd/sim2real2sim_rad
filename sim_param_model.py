7f3ecf817eb037e1f3ccbb42f35e8c6ce8c2ed0a
AliengirlLiv <oliviagracewatkins@gmail.com>
2020-10-14 11:27:22 -0700

    move pring

--- sim_param_model.py
+++ sim_param_model.py
@@ -111,8 +111,8 @@ class SimParamModel(nn.Module):
                 # Input is b x c * num_frames x h x w.  We take the first image and downsample to
                 downsampled_img = input[:, 0, ::25, ::25].reshape(len(input), -1)
                 features = downsampled_img
+                #features = self.encoder(input, detach=False)
                 print("NORM", torch.norm(features).item(), torch.min(features).item(), torch.max(features).item())
-                # features = self.encoder(input, detach=False)
                 # features = features / torch.norm(features)
             else:
                 if type(obs_traj[0][0]) is torch.Tensor:
