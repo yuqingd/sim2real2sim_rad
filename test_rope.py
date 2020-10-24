7948f88d8d77554b2de1f7f3db55f74df672ebeb
AliengirlLiv <oliviagracewatkins@gmail.com>
2020-10-23 23:37:47 -0700

    easy push (block and goal are directly in line)

--- test_rope.py
+++ test_rope.py
@@ -65,68 +65,11 @@ args = parse_args()
 
 ######## TEST CABINET
 
-env = make('kitchen', real_world=False,
-        task_name='real_c',
-        seed=0,
-        height=512,
-        width=512,
-        dr_list=args.real_dr_list,
-        dr_shape=args.sim_params_size,
-        dr=args.dr,
-        mean_only=args.mean_only,
-    )
-# env.set_special_reset('grip')
-env.reset()
-env.apply_dr()
-num_episodes = 1
-time_limit = 60
-image_size = 512
-real_video_dir = utils.make_dir(os.path.join('./logdir', 'debug_video'))
-
-video = VideoRecorder(real_video_dir, camera_id=0, height=512, width=512)
-
-cpf = 3
-obs_shape = (cpf * 3, image_size, image_size)
-action_shape = env.action_space.shape
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-def run_eval_loop(sample_stochastically=True):
-    for i in range(num_episodes):
-        obs_dict = env.reset()
-        video.init(enabled=(i == 0))
-        video.record(env)
-        done = False
-        episode_reward = 0
-        obs_traj = []
-        step = 0
-        while not done and step < time_limit:
-            obs = obs_dict['image']
-            # center crop image
-            obs = utils.center_crop_image(obs, image_size)
-            if step < 20:
-                action = [.3, .3, .6]
-            elif step < 40:
-                action = [-.3, .1, -.2]
-            else:
-                action = [-.3, 0, -.1]
-
-            step+=1
-            obs_traj.append(obs)
-            obs_dict, reward, done, info = env.step(action)
-            video.record(env)
-            episode_reward += reward
-            print(reward)
-        video.save('sim_cabinet.mp4')
-
-run_eval_loop()
-
-# ######## TEST PUSH
-#
 # env = make('kitchen', real_world=False,
-#         task_name='real_p',
+#         task_name='real_c',
 #         seed=0,
-#         height=84,
-#         width=84,
+#         height=512,
+#         width=512,
 #         dr_list=args.real_dr_list,
 #         dr_shape=args.sim_params_size,
 #         dr=args.dr,
@@ -135,13 +78,12 @@ run_eval_loop()
 # # env.set_special_reset('grip')
 # env.reset()
 # env.apply_dr()
-# print(env.goal)
 # num_episodes = 1
-# time_limit = 1
-# image_size = 84
+# time_limit = 60
+# image_size = 512
 # real_video_dir = utils.make_dir(os.path.join('./logdir', 'debug_video'))
 #
-# video = VideoRecorder(real_video_dir, camera_id=0)
+# video = VideoRecorder(real_video_dir, camera_id=0, height=512, width=512)
 #
 # cpf = 3
 # obs_shape = (cpf * 3, image_size, image_size)
@@ -152,6 +94,7 @@ run_eval_loop()
 #     for i in range(num_episodes):
 #         obs_dict = env.reset()
 #         video.init(enabled=(i == 0))
+#         video.record(env)
 #         done = False
 #         episode_reward = 0
 #         obs_traj = []
@@ -160,13 +103,74 @@ run_eval_loop()
 #             obs = obs_dict['image']
 #             # center crop image
 #             obs = utils.center_crop_image(obs, image_size)
-#             action = [0., 1, 0 ]
+#             if step < 20:
+#                 action = [.3, .3, .6]
+#             elif step < 40:
+#                 action = [-.3, .1, -.2]
+#             else:
+#                 action = [-.3, 0, -.1]
+#
 #             step+=1
 #             obs_traj.append(obs)
 #             obs_dict, reward, done, info = env.step(action)
 #             video.record(env)
 #             episode_reward += reward
 #             print(reward)
-#         video.save('sim_push.mp4')
+#         video.save('sim_cabinet.mp4')
 #
 # run_eval_loop()
+
+######## TEST PUSH
+
+image_size = 480
+env = make('kitchen', real_world=False,
+        task_name='real_p',
+        seed=0,
+        height=image_size,
+        width=image_size,
+        dr_list=args.real_dr_list,
+        dr_shape=args.sim_params_size,
+        dr=args.dr,
+        mean_only=args.mean_only,
+    )
+# env.set_special_reset('grip')
+env.reset()
+env.apply_dr()
+print(env.goal)
+num_episodes = 1
+time_limit = 60
+real_video_dir = utils.make_dir(os.path.join('./logdir', 'debug_video'))
+
+video = VideoRecorder(real_video_dir, camera_id=0)
+
+cpf = 3
+obs_shape = (cpf * 3, image_size, image_size)
+action_shape = env.action_space.shape
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def run_eval_loop(sample_stochastically=True):
+    for i in range(num_episodes):
+        obs_dict = env.reset()
+        video.init(enabled=(i == 0))
+        done = False
+        episode_reward = 0
+        obs_traj = []
+        step = 0
+        while not done and step < time_limit:
+            obs = obs_dict['image']
+            # center crop image
+            obs = utils.center_crop_image(obs, image_size)
+            if step < 10:
+                action = [0., 0., -1]
+            else:
+                action = [0, 1, 0]
+
+            step+=1
+            obs_traj.append(obs)
+            obs_dict, reward, done, info = env.step(action)
+            video.record(env)
+            episode_reward += reward
+            print(reward)
+        video.save('sim_push.mp4')
+
+run_eval_loop()
